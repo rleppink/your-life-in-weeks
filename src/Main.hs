f566fec71aef6c8a3222941f6c1ea19fa4e32fa5
Ronald Leppink <ronaldleppink@gmail.com>
2017-11-15 21:13:14 +0100

    Add preliminary arg work, need to make it work with mainWith

--- src/Main.hs
+++ src/Main.hs
@@ -1,48 +1,56 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE TypeFamilies              #-}
 
 module Main where
 
 
-import Data.Time
+import           Data.Time
 
-import Diagrams.Prelude
-import Diagrams.Backend.Rasterific.CmdLine
+import           Diagrams.Backend.Rasterific.CmdLine
+import           Diagrams.Prelude
+
+import           System.Environment
 
 
 main :: IO ()
 main = do
   today <- fmap utctDay getCurrentTime
+  args <- getArgs
 
-  let dayDiff = diffDays today (fromGregorian 1989 03 12)
-  let weeksAlive = weeks $ fromIntegral dayDiff
+  let dateArg =
+        parseTimeOrError
+            True
+            defaultTimeLocale
+            (iso8601DateFormat Nothing)
+            (head args)
+            :: Day
 
-  mainWith $ lifeDiagram weeksAlive # bgFrame 1 white
+
+  let dayDiff = fromIntegral $ diffDays today dateArg
+
+  mainWith $
+    lifeDiagram (weeks dayDiff) 90 # bgFrame 1 white
 
 
-lifeDiagram :: Int -> Diagram B
-lifeDiagram x =
+lifeDiagram :: Int -> Int -> Diagram B
+lifeDiagram x y =
     vsep sepDist $
-        replicate (filledYears x) (hsep sepDist filledYear) ++
+        replicate (yearsAlive x) (hsep sepDist filledYear) ++
         [hsep sepDist (partialYear (mod x 52))] ++
-        replicate (emptyYears x) (hsep sepDist emptyYear)
+        replicate (yearsMax x y) (hsep sepDist emptyYear)
 
 weeks :: Int -> Int
-weeks x
-  | remainder > 3 = divided + 1
-  | otherwise     = divided
-  where divided   = fst $ divMod x 7
-        remainder = snd $ divMod x 7
+weeks x = div x 7
 
 sepDist :: Double
 sepDist = 0.8
 
-filledYears :: Int -> Int
-filledYears x = div x 52
+yearsAlive :: Int -> Int
+yearsAlive x = div x 52
 
-emptyYears :: Int -> Int
-emptyYears x = div ((90 * 52) - x) 52
+yearsMax :: Int -> Int -> Int
+yearsMax x y = div ((y * 52) - x) 52
 
 filledYear :: [Diagram B]
 filledYear = replicate 52 filledSquare
